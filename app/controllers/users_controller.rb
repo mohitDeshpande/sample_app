433cbae8839c5623944178a0a7179abebf7af379
Mohit Deshpande <mohitdeshpande@icloud.com>
2013-12-18 08:11:34 +0530

    Finish user edit, update and delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
   
   def new
   	@user = User.new
+  end
+  
+  def index
+  	@user = User.paginate(page: params[:page])
   end
   
   def create
@@ -19,9 +26,48 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
   
+  def edit
+  end
+  
+  def update
+  	if @user.update_attributes(user_params)
+  		# Handle successful update
+  		flash[:success] = "Profile Updated"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+  
+  def destroy
+  	User.find(params[:id]).destroy!
+  	flash[:success] = "User deleted"
+  	redirect_to users_url
+  end
+  
+  	
+  
   private
-  def user_params
+  	
+  	def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-  
+	
+		# Before filters
+		def signed_in_user
+			unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+		end
+		
+		def correct_user
+			@user = User.find_by(params[:id])
+			redirect_to(root_url) unless current_user?(@user)
+		end 
+		
+		def admin_user
+			redirect_to(root_url) unless current_user.admin?
+		end 
+
 end
